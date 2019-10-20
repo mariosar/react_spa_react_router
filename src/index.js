ebd6e132eb247f96260492bcb93c621d59e5eb22
Mario Saraiva <mario.s.saraiva@gmail.com>
2019-10-20 00:03:17 -0400

    Removed and cleaned out files to start fresh.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styles from './index.module.css';
-import './index.scss';
-import mario from './public/images/super_mario.png';
 
 class App extends React.Component{
     render(){
         return(
-            <div className={styles.appContainer}>
-                <div className="dashed-class">Should be line-through</div>
-                Hello World!
-                <img src={mario} alt="Logo" />;
+            <div>
+                Hello World
             </div>
         )
     }
